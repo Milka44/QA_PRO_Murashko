356858b58f6a5c1cf16d8adb5c6bc7c8ad00d7d8
Milka44 <po4tafor4@gmail.com>
2023-12-16 11:12:49 +0200

    Some changes added

--- HW9.js
+++ HW9.js
@@ -1,15 +1,15 @@
 //ДЗ 1.Var 1-----
 
-// function myBlend(arr) {
-//     for (let i = arr.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         
-//         [arr[i], arr[j]] = [arr[j], arr[i]];
-//     }
-// }
+function myBlend(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
 
-// let arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 105];
-// myBlend(arr);
+let arr =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 105];
+myBlend(arr);
 
 // console.log(arr); 
 
